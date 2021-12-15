13bba570bad35bba136b2c7b13b7b1c3007b1048
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-12-15 17:01:06 +0100

    iniziate query di tipo "JOIN"

--- query-join.sql
+++ query-join.sql
@@ -1,8 +1,16 @@
 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
-
+SELECT `students`.`degree_id`AS`id-corso`, `students`.`id`AS`id-studente`, `degrees`.`name`AS`corso`
+FROM `students` 
+INNER JOIN `degrees`
+ON `students`.`degree_id`=`degrees`.`id`
+WHERE `degrees`.`name`LIKE"%laurea in economia%"
 
 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
-
+SELECT `degrees`.`department_id`AS`id-dipartimento-del-corso`, `degrees`.`id`, `degrees`.`name`AS`nome-del-corso`, `departments`.`name`AS`dipartimento`
+FROM `degrees`
+INNER JOIN `departments`
+ON `degrees`.`department_id`= `departments`.`id`
+WHERE `departments`.`name`LIKE"%di neuroscienze%"
 
 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
@@ -14,7 +22,7 @@ relativo dipartimento, in ordine alfabetico per cognome e nome
 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
 
-6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica
 
 
 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
